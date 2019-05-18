59425510adfbbc42fe86114808dbfad2aa1f583e
alex.chiu <alex.chiu127@gmail.com>
2019-05-18 22:05:19 +0800

    修改「客戶資料」表格，新增「客戶分類」欄位

---
+++ CustomerManagement/Models/客戶類型Enum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CustomerManagement.Models
+{
+
+    public enum 客戶分類Enum
+    {
+        VIP,
+        Normal
+    }
+}
