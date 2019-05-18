59a320948d8f3f65330d8dd7d433df49bae3070f
alex.chiu <alex.chiu127@gmail.com>
2019-05-18 22:51:25 +0800

    利用jquery datatable, 修改「客戶資料列表」與「客戶聯絡人列表」頁面，設定讓每個欄位都能進行排序 (可升冪、可降冪排序)

--- CustomerManagement/App_Start/BundleConfig.cs
+++ CustomerManagement/App_Start/BundleConfig.cs
@@ -14,6 +14,9 @@ namespace CustomerManagement
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jqueryDataTables").Include(
+                        "~/Scripts/DataTables/jquery.dataTables.js"));
+
             // 使用開發版本的 Modernizr 進行開發並學習。然後，當您
             // 準備好可進行生產時，請使用 https://modernizr.com 的建置工具，只挑選您需要的測試。
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -24,7 +27,8 @@ namespace CustomerManagement
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/DataTables/css/jquery.dataTables.css"));
         }
     }
 }
