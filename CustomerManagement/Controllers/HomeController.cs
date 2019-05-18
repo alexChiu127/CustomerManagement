a29fb8b2892654e83657f37d1156205ba98a6f6f
alex.chiu <alex.chiu127@gmail.com>
2019-05-18 18:16:38 +0800

    實作一個清單頁面，

--- CustomerManagement/Controllers/HomeController.cs
+++ CustomerManagement/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CustomerManagement.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,11 +7,15 @@ using System.Web.Mvc;
 
 namespace CustomerManagement.Controllers
 {
+
     public class HomeController : Controller
     {
+        private 客戶資料Entities db = new 客戶資料Entities();
+
         public ActionResult Index()
         {
-            return View();
+            var result = db.客戶管理清單.AsQueryable();
+            return View(result);
         }
 
         public ActionResult About()
